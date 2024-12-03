000108da0b04b5f0ac61bbdcb4f56a63ec5bce1a
Shrihari Mahabal <shriharimahabal2@gmail.com>
2024-12-03 11:47:14 +0530

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router' 
+import "../global.css";
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text className='text-3xl text-red-500'>RootLayout</Text>
+    </View>
+  )
+}
+
+export default RootLayout
